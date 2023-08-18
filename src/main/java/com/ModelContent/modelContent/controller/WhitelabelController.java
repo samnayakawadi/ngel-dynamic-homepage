a83af668423c04bfb04770c98bc3175b57da1ed3
Samir Nayakawadi <samnayakawadi@gmail.com>
2023-08-18 17:22:30 +0530

    Added Whitelable Error Pages

---
+++ src/main/java/com/ModelContent/modelContent/controller/WhitelabelController.java
@@ -0,0 +1,32 @@
+package com.ModelContent.modelContent.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WhitelabelController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if (statusCode == HttpStatus.UNAUTHORIZED.value()) {
+                return "error-unauthorized";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500";
+            } else if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";
+            }
+        }
+        return "error";
+    }
+
+}
